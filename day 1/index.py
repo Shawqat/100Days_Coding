0ce1760b7519c202e1a6490edf1947277761679e
Mahmoud_Shawqat <mahmoud.k.shawqat@gmail.com>
2025-09-25 05:29:07 +0300

    ADD: First Day Code.

---
+++ day 1/index.py
@@ -0,0 +1,14 @@
+# first line of code printing to screen
+print("Welcome, printing to screen using python!")
+
+# Variables, Data Types, and Input/Output
+# Data types : int, float, string, boolean
+name        = "Mahmoud" # string
+age         = 29 # int
+weight      = 82.5 # float
+is_online   = True # bool
+
+# Input/Output -> to get data from user to process it.
+user_name = input("Enter your email: ")
+print("Welcome,", user_name)
+
---
+++ day 1/task.py
@@ -0,0 +1,11 @@
+# Create a file called user_info.py.
+# Write a program that:
+# Asks the user for their name and age.
+# Stores this information in variables.
+# Prints a message like:
+# Hello Shawqat, you are 25 years old! (replace with your name and age).
+
+user_name = input("Enter your name: ")
+user_age  = input("Enter your age: ")
+
+print("Hello,", user_name, "you are", user_age, "years old!")
